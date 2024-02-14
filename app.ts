99151dda95c60abf352609fb592eab35186efa29
Nikita Migushev <migushev@gmail.com>
2024-02-14 11:34:50 +0300

    practice typescript

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('Your code goes here...');
+function add (n1: number, n2: number, showResult: boolean, phrase: string) {
+    if (showResult) {
+        console.log(`${resultPhrase} ${n1 + n2}`);
+    } else return n1 + n2;
+}
+
+const num1 = 5;
+const num2 = 1;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+const result = add(num1, num2, printResult, resultPhrase);
+

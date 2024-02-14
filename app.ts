a495100d7f953e3293ccb5446129b9e050f47613
Nikita Migushev <migushev@gmail.com>
2024-02-14 12:06:10 +0300

    practice typescript

--- app.ts
+++ app.ts
@@ -11,3 +11,17 @@ const resultPhrase = 'Result is: ';
 
 const result = add(num1, num2, printResult, resultPhrase);
 
+const person: {
+    name: string,
+    age: number,
+    hobbies: string[],
+    role: 'READ-ONLY-USER'
+} = {
+    name: 'Max',
+    age: 30,
+    hobbies: ['A', 'B', 'C'],
+    role: 'READ-ONLY-USER'
+};
+
+console.log(person.toString())
+
